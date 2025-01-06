c3d0da8df54661f49daca601aa6d71ecf23327ac
ABHASH KUMAR DAS <108912904+abhash29@users.noreply.github.com>
2025-01-06 22:32:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 
+let purchased = [];
+
 // 1. Admins should be able to sign up
 app.post('/admin/signup', (req, res) => {
   const admin = req.body;
@@ -66,7 +68,6 @@ app.post('/admin/login', adminAuthentication, (req, res) => {
 
 
 
-//Yaha se check karna hai
 //3. Admin courses ko daal sakega
 app.post('/admin/courses', adminAuthentication, (req, res) => {
   const course = req.body;
@@ -75,8 +76,7 @@ app.post('/admin/courses', adminAuthentication, (req, res) => {
     if(err) throw err;
     const COURSES = JSON.parse(data);
     COURSES.push(course);
-    
-
+  
     fs.writeFile('courses.json', JSON.stringify(COURSES), (err) => {
       if(err) throw err;
     res.status(200).json({message: "Course added successfully"});
@@ -84,6 +84,28 @@ app.post('/admin/courses', adminAuthentication, (req, res) => {
   });
 });
 //4. Admin courses mai changes la sakega
+app.put('/admin/courses/:id', adminAuthentication, (req, res) => {
+  fs.readFile('courses.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    const COURSES = JSON.parse(data);
+    const id = parseInt(req.params.id);
+
+    const courseIndex = COURSES.findIndex(course => course.id === id);
+    if (courseIndex !== -1) {
+      const course = COURSES[courseIndex];
+      Object.assign(course, req.body);
+
+      fs.writeFile('courses.json', JSON.stringify(COURSES, null, 2), (err) => {
+        if (err) throw err;
+        res.status(200).json({ message: "Updated successfully" });
+      });
+    } else {
+      res.status(404).json({ message: "Course not found" });
+    }
+  });
+});
+
+
 //5. Admin courses ka list to get kar sakega
 app.get('/admin/courses', adminAuthentication, (req, res) => {
   fs.readFile('courses.json', 'utf-8', (err, data) => {
@@ -94,8 +116,106 @@ app.get('/admin/courses', adminAuthentication, (req, res) => {
 });
 
 //6. Admin courses ko delete v kar sakta hai
-app.delete('')
+app.delete('/admin/courses/:id', adminAuthentication, (req, res) => {
+  fs.readFile('courses.json', 'utf-8', (err, data) => {
+    if(err) throw err;
+    const COURSES = JSON.parse(data);
+    const id = req.params.id;
 
+    const newCOURSES = COURSES.filter(course => course.id!=id);
+
+    fs.writeFile('courses.json', JSON.stringify(newCOURSES), (err) => {
+      if(err) throw err;
+      res.status(200).json({message: "Course Deleted successfully"});
+    });  
+  });
+});
+
+
+
+//Users ke baare mai kaam karna hai
+//1. User signup
+app.post('/user/signup', (req, res) => {
+  fs.readFile('users.json', 'utf-8', (err, data) => {
+    if(err) throw err;
+    const USER = JSON.parse(data);
+    
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      purchasedCOurses: []
+    }
+
+    USER.push(user);
+    fs.writeFile('users.json', JSON.stringify(USER), (err) => {
+      if(err) throw err;
+      res.status(200).json({message: "User created successfully"});
+    })
+  })
+})
+
+//2. user login
+//3. login ke liye middlewware
+const userAuthentication = (req, res, next) => {
+  const {username, password} = req.headers;
+  fs.readFile('users.json', 'utf-8', (err, data) => {
+    if(err) throw err;
+    const USER = JSON.parse(data);
+    const user = USER.find(a => a.username===username && a.password===password);
+    if(user){
+      return next();
+    }
+    else{
+      res.status(403).json({message: "User Authentication failed"});
+    }
+  });
+}
+app.post('/user/login', userAuthentication, (req, res) => {
+  res.status(200).json({message: "user login successful"});
+})
+
+//4. Users course dekh sakte h
+app.get('/user/courses', userAuthentication, (req, res) => {
+  fs.readFile('courses.json', 'utf-8', (err, data) => {
+    if(err) throw err;
+    const COURSES = JSON.parse(data);
+    res.status(200).json({courses: COURSES});
+  });
+});
+
+
+//5. Users course ko purchased mai add kar sakte h
+app.post('/user/courses/:id', userAuthentication, (req, res) => {
+  const id = parseInt(req.params.id);
+  fs.readFile('courses.json', 'utf-8', (err, data) => {
+    if(err) throw err;
+    const COURSES = JSON.parse(data);
+    const alreadyPurchased = purchased.find(a => a.id===id);
+    const buy = COURSES.find(a => a.id===id);
+    if(!alreadyPurchased && buy){  
+      purchased.push(buy);
+      res.status(200).json({message: "Coursed bought successfully"});
+    }
+    else{
+      res.status(404).json({message: "Already purchased"});
+    }
+  });
+});
+//6. Purchased ka list
+app.get('/users/purchasedCourses', userAuthentication, (req, res) => {
+  const purchasedCourseIds = req.user.purchasedCourses; // e.g., [1, 4]
+  
+  fs.readFile('courses.json', 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: "Error reading courses file", error: err.message });
+    }
+
+    const COURSES = JSON.parse(data);
+    const purchasedCourses = COURSES.filter(course => purchasedCourseIds.includes(course.id));
+
+    res.json({ purchasedCourses });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Port is running at ${port}`);
